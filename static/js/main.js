27206916748644d5b3f55b1b6001ff88393f989a
thuitafaith <thuitamuthoni15@gmail.com>
2018-06-05 19:08:25 +0300

    Create javascript files

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+(function($) {
+
+  "use strict";
+
+
+  $('.navigation').singlePageNav({
+    currentClass: 'active'
+  });
+
+
+  $('.toggle-menu').click(function() {
+    $('.responsive-menu').stop(true, true).slideToggle();
+    return false;
+  });
+
+
+
+})(jQuery);

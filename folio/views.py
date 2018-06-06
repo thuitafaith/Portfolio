23eb0f35743aa91fa6f94e997cbaa054546bd17b
thuitafaith <thuitamuthoni15@gmail.com>
2018-06-06 08:51:25 +0300

    Add images

--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Project,Skill,Personal_Information
 # Create your views here.
+def home(request):
+    title = "FAITH THUITA"
+    information = Personal_Information.objects.all()
+    skills = Skill.objects.all()
+    projects = Project.objects.all()
+    for info in information:
+        information = info
+    return render(request, 'home.html', {'title': title, 'projects': projects,
+                                          'information': information, 'skills': skills})

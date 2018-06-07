29a42012be26b25fec3c3e72f8a82a9e23b78bdf
thuitafaith <thuitamuthoni15@gmail.com>
2018-06-07 10:37:56 +0300

    Add new images

--- folio/views.py
+++ folio/views.py
@@ -3,10 +3,10 @@ from .models import Project,Skill,Personal_Information
 # Create your views here.
 def home(request):
     title = "FAITH THUITA"
-    information = Personal_Information.objects.all()
+    information = Personal_Information.objects.first()
     skills = Skill.objects.all()
     projects = Project.objects.all()
-    for info in information:
-        information = info
+    # for info in information:
+    #     information = info
     return render(request, 'home.html', {'title': title, 'projects': projects,
                                           'information': information, 'skills': skills})
